2b93aecf30ea28d08a475b3baad1db57d12bc7a4
olojamuyiwa <olojam4969@gmail.com>
2024-09-17 11:56:24 +0700

    email domain configuration

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.EMAIL_API_SECRET);
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.DEV_URL}/new-verification?token=${token}`;
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'mail@proximaheritagecrest.org',
     to: email,
     subject: 'Confirm your email',
     html: `
@@ -21,7 +21,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${process.env.DEV_URL}/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: 'onboarding@resend.dev',
+    from: 'mail@proximaheritagecrest.org',
     to: email,
     subject: 'Reset your password',
     html: `
